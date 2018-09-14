9feed63ebbda70b1c0b1f0c28d52867346885516
YangKnkp <yangknkp@email.com>
2018-09-14 12:09:22 +0800

    封装跳转方法

--- src/App.vue
+++ src/App.vue
@@ -23,4 +23,57 @@ export default {
   font-size: 30rpx;
   font-family: "Arial","Microsoft YaHei","黑体","宋体",sans-serif;
 }
+
+/* search Start */
+.bg_search {
+  background: #FFFFFF;
+  top: 0rpx !important;
+}
+
+.search {
+  width: 100%;
+  position: fixed;
+  top: 5rpx;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
+  z-index: 999;
+}
+
+.search_input_icon {
+  width: 80%;
+}
+
+.search_cion {
+  margin: 10rpx 10rpx 10rpx 10rpx;
+  position: absolute;
+  left: 100rpx;
+  width: 20rpx;
+  height: 20rpx;
+  text-align: center;
+}
+
+.search_input {
+  border: 2rpx solid #d0d0d0;
+  background: #FFFFFF;
+  border-radius: 10rpx;
+  margin-left: 20rpx;
+}
+
+.search_input input {
+  margin-left: 60rpx;
+  height: 60rpx;
+  border-radius: 10rpx;
+  font-size: 30rpx;
+}
+
+.search_button {
+  margin-left: 15rpx;
+  width: 15%;
+  line-height: 150%;
+  text-align: center;
+  border: 2rpx solid #d0d0d0;
+  border-radius: 10rpx;
+}
+/* search End */
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App'
 import http from './utils/http'
+import to from './utils/to'
 
 Vue.config.productionTip = false
 Vue.prototype.$http = http
+Vue.prototype.$to = to
 App.mpType = 'app'
 
 const app = new Vue(App)
